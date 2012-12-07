502fca8e90bba0f1e3a9397acaaae3b0c17b5604
Vincent Matte-Deschênes <scootersbooms932@hotmail.com>
2012-12-07 15:17:05 -0500

    Ajout des test Memento
    Pour facilité la compréhension
    Il faut l'enlever à la remise

---
+++ tp3/TP3/src/tp/TestMemento.java
@@ -0,0 +1,25 @@
+package tp;
+
+public class TestMemento {
+	
+	public static void test(){
+		PileMemento pileMemento = new PileMemento();
+		CreateurMemento createurMemento = new CreateurMemento();
+	
+		
+		//Ajout des attribut du memento
+		createurMemento.nouveauMemento(100, 15, 50);
+		
+		//Ajout de l'état 
+		pileMemento.ajouterMemento(createurMemento.sauverDansMemento());
+		
+		//Nouveau Memento
+		createurMemento.nouveauMemento(75, 15, 50);
+		
+		//Recupétation du premier memento (index 0)
+		Memento vieuxMemento = pileMemento.getMemento(0);
+		
+		//Ajouter le vieuxMemento comme Memento actuel
+		createurMemento.restaurerDepuisMemento(vieuxMemento);
+	}
+}
